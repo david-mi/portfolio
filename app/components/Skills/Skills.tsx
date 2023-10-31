44c326701d141aa54f2966ed6c043afe47b69462
David Michel <david.michel2809@gmail.com>
2023-10-31 18:57:17 +0100

    feat/refacto: improve skills style and add icons for each skill

--- app/components/Skills/Skills.tsx
+++ app/components/Skills/Skills.tsx
@@ -1,3 +1,17 @@
+import {
+  ExpressJsIcon,
+  FigmaIcon,
+  FirebaseIcon,
+  GitIcon,
+  GithubIcon,
+  NextJsIcon,
+  ReactIcon,
+  ReduxIcon,
+  TypescriptIcon,
+  MongoDbIcon,
+  MysqlIcon
+}
+  from "@/icons"
 import styles from "./skills.module.css";
 
 function Skills() {
@@ -9,29 +23,29 @@ function Skills() {
         <section>
           <h4>Front End</h4>
           <ul className={styles.skillsList}>
-            <li>NextJs</li>
-            <li>React</li>
-            <li>TypeScript</li>
-            <li>HTML</li>
-            <li>CSS</li>
+            <li><NextJsIcon className={styles.svg} />NextJs</li>
+            <li><ReduxIcon className={styles.svg} />Redux</li>
+            <li><ReactIcon className={styles.svg} />React</li>
+            <li><TypescriptIcon className={styles.svg} />TypeScript</li>
           </ul>
         </section>
 
         <section>
           <h4>Back End</h4>
           <ul className={styles.skillsList}>
-            <li>Express</li>
-            <li>MongoDB</li>
-            <li>MySQL</li>
+            <li><ExpressJsIcon className={styles.svg} />ExpressJs</li>
+            <li><MongoDbIcon className={styles.svg} /> MongoDB</li>
+            <li><MysqlIcon className={styles.svg} /> MySQL</li>
           </ul>
         </section>
 
         <section>
           <h4>Divers</h4>
           <ul className={styles.skillsList}>
-            <li>Git / Github</li>
-            <li>Firebase</li>
-            <li>Figma</li>
+            <li><GitIcon className={styles.svg} />Git</li>
+            <li><GithubIcon className={styles.svg} />Github</li>
+            <li><FirebaseIcon className={styles.svg} />Firebase</li>
+            <li><FigmaIcon className={styles.svg} />Figma</li>
           </ul>
         </section>
       </div>
