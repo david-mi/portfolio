d3028ac6fc3ec42a4ba05a8cc93395a9ce5bdd11
David Michel <david.michel2809@gmail.com>
2023-11-05 09:16:20 +0100

    refacto: rename Projets title to Projets Persos

--- app/components/Projects/Projects.tsx
+++ app/components/Projects/Projects.tsx
@@ -6,7 +6,7 @@ import styles from "./projects.module.css"
 function Projects() {
   return (
     <section className={styles.container}>
-      <h3 className={styles.title}>Projets</h3>
+      <h3 className={styles.title}>Projets Persos</h3>
       <div className={styles.projects}>
         {projectsData.map((project) => {
           return <ProjectCard key={project.id} {...project} />
