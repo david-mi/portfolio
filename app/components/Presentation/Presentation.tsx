23f072e9d4af2a723ce33b9c5c4ee4913b42dd74
David Michel <david.michel2809@gmail.com>
2023-11-22 09:26:06 +0100

    refacto/style: rework presentation

--- app/components/Presentation/Presentation.tsx
+++ app/components/Presentation/Presentation.tsx
@@ -21,29 +21,37 @@ function Presentation() {
         className={styles.picture}
         priority
       />
-      <p className={styles.description}>
-        Fraîchement diplômé d'une formation de
-        Développeur Web
-        <Link
-          href="https://openclassrooms.com/fr/paths/717-developpeur-web"
-          target="_blank"
-          aria-label="Visiter la page de formation Développeur Web"
-          title="Visiter la page de formation Développeur Web"
-        >
-          <ExternalLink className={styles.svg} />
-        </Link>
-        puis une seconde de
-        Développeur d'application JavaScript React
-        <Link
-          href="https://openclassrooms.com/fr/paths/877-developpeur-dapplication-javascript-react"
-          target="_blank"
-          aria-label="Visiter la page de formation Développeur d'application JavaScript React"
-          title="Visiter la page de formation Développeur d'application JavaScript React"
-        >
-          <ExternalLink className={styles.svg} />
-        </Link>
+      <article className={styles.description}>
+        <span>Diplômé de 2 formations :</span>
+        <ul>
+          <li>
+            <span>Développeur Web</span>
+            <Link
+              href="https://openclassrooms.com/fr/paths/717-developpeur-web"
+              target="_blank"
+              aria-label="Visiter la page de formation Développeur Web"
+              title="Visiter la page de formation Développeur Web"
+            >
+              <ExternalLink className={styles.svg} />
+            </Link>
+          </li>
+          <li>
+            <span>Développeur d'application</span>
+            <span>
+              JavaScript React
+              <Link
+                href="https://openclassrooms.com/fr/paths/877-developpeur-dapplication-javascript-react"
+                target="_blank"
+                aria-label="Visiter la page de formation Développeur d'application JavaScript React"
+                title="Visiter la page de formation Développeur d'application JavaScript React"
+              >
+                <ExternalLink className={styles.svg} />
+              </Link>
+            </span>
+          </li>
+        </ul>
         Je souhaite maintenant trouver une entreprise pour collaborer.
-      </p>
+      </article>
     </header>
   );
 }
