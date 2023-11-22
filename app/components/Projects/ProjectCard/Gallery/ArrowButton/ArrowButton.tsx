47d765a6daa2d8cd9377cab3622e28b9e15fe7e2
David Michel <david.michel2809@gmail.com>
2023-11-22 09:22:25 +0100

    refacto/style: change style around ModaleLayout and Gallery

--- app/components/Projects/ProjectCard/Gallery/ArrowButton/ArrowButton.tsx
+++ app/components/Projects/ProjectCard/Gallery/ArrowButton/ArrowButton.tsx
@@ -12,8 +12,10 @@ function ArrowButton({ onClick, role }: Props) {
   return (
     <button onClick={onClick} className={classNames}>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-        <path fill="currentColor" fillRule="evenodd" d="M3.464 20.535C4.93 22 7.286 22 12 22c4.714 0 7.071 0 8.535-1.465C22 19.072 22 16.714 22 12s0-7.071-1.465-8.536C19.072 2 16.714 2 12 2S4.929 2 3.464 3.464C2 4.93 2 7.286 2 12c0 4.714 0 7.071 1.464 8.535ZM9.97 15.53a.75.75 0 0 1 0-1.06L12.44 12L9.97 9.53a.75.75 0 0 1 1.06-1.06l3 3a.75.75 0 0 1 0 1.06l-3 3a.75.75 0 0 1-1.06 0Z" clipRule="evenodd">
-        </path>
+        <g fill="none" stroke="currentColor" stroke-width="1.5">
+          <circle cx="12" cy="12" r="10"></circle>
+          <path stroke-linecap="round" stroke-linejoin="round" d="m13.5 9l-3 3l3 3"></path>
+        </g>
       </svg>
     </button>
   );
