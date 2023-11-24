7720b00580d96e3fb2dde788ddf5e3a61188d209
David Michel <david.michel2809@gmail.com>
2023-11-24 09:25:02 +0100

    style: add appearance effect on Skills categories
    torebase: appearance skill categories

--- app/components/Skills/Category/Category.tsx
+++ app/components/Skills/Category/Category.tsx
@@ -1,3 +1,6 @@
+"use client"
+
+import { useEffect, useRef } from "react";
 import { PropsWithChildren } from "react"
 import styles from "./category.module.css";
 
@@ -6,8 +9,21 @@ interface Props extends PropsWithChildren {
 }
 
 function Category({ title, children }: Props) {
+  const sectionElementRef = useRef<HTMLElement>(null!)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      if (!entry.isIntersecting) return
+
+      sectionElementRef.current.classList.add(styles.show)
+      observer.unobserve(sectionElementRef.current)
+    }, { threshold: 0.3 })
+
+    observer.observe(sectionElementRef.current)
+  }, [])
+
   return (
-    <section className={styles.category}>
+    <section className={styles.category} ref={sectionElementRef}>
       <h4>{title}</h4>
       <ul className={styles.skillsList}>
         {children}
