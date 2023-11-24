191d6701a21bd22b33384ec0845005d2ee74b991
David Michel <david.michel2809@gmail.com>
2023-11-24 08:41:58 +0100

    style: add appearance effect on section titles

--- app/components/SectionTitle/SectionTitle.tsx
+++ app/components/SectionTitle/SectionTitle.tsx
@@ -1,3 +1,6 @@
+"use client"
+
+import { useEffect, useRef } from "react";
 import styles from "./sectionTitle.module.css";
 
 interface Props {
@@ -5,9 +8,26 @@ interface Props {
 }
 
 function SectionTitle({ title }: Props) {
+  const titleElementRef = useRef<HTMLHeadingElement>(null!)
+
+  const titleFirstLetter = title[0]
+  const restOfTitle = title.slice(1)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      if (!entry.isIntersecting) return
+
+      titleElementRef.current.classList.add(styles.show)
+      observer.unobserve(titleElementRef.current)
+    })
+
+    observer.observe(titleElementRef.current)
+  }, [])
+
   return (
-    <h3 className={styles.sectionTitle}>
-      <span>{title}</span>
+    <h3 className={styles.sectionTitle} ref={titleElementRef}>
+      <span>{titleFirstLetter}</span>
+      <span>{restOfTitle}</span>
     </h3>
   );
 }
