107a2cb21539a6b526f89b87956101a1f2d89f8d
David Michel <david.michel2809@gmail.com>
2023-11-24 11:27:11 +0100

    style: add appearance effects on Social part

--- app/components/Social/SocialLink/SocialLink.tsx
+++ app/components/Social/SocialLink/SocialLink.tsx
@@ -1,10 +1,27 @@
+"use client"
+
+import { useEffect, useRef } from "react";
 import { SocialData } from "../data";
 import styles from "./socialLink.module.css";
 import Link from "next/link";
 
 function SocialLink({ name, href, label, icon }: SocialData) {
+  const linkElementRef = useRef<HTMLAnchorElement>(null!)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      if (!entry.isIntersecting) return
+
+      linkElementRef.current.classList.add(styles.show)
+      observer.unobserve(linkElementRef.current)
+    }, { threshold: 0.3 })
+
+    observer.observe(linkElementRef.current)
+  }, [])
+
   return (
     <Link
+      ref={linkElementRef}
       className={styles.socialLink}
       href={href}
       target="_blank"
@@ -12,7 +29,7 @@ function SocialLink({ name, href, label, icon }: SocialData) {
       title={label}
     >
       {icon}
-      <p>{name}</p>
+      <p className={styles.name}>{name}</p>
     </Link>
   );
 }
