30d4414db39eceaf0e78bdd1236800cd5c7607cf
David Michel <david.michel2809@gmail.com>
2023-12-28 07:33:28 +0100

    refacto: rework presentation

--- app/components/Presentation/Presentation.tsx
+++ app/components/Presentation/Presentation.tsx
@@ -1,8 +1,6 @@
 import Image from "next/image";
 import picture from "@/public/david-picture.png"
-import Link from "next/link";
 import styles from "./presentation.module.css";
-import ExternalLink from "../icons/ExternalLink";
 
 function Presentation() {
   return (
@@ -21,37 +19,6 @@ function Presentation() {
         className={styles.picture}
         priority
       />
-      <article className={styles.description}>
-        <span>Diplômé de 2 formations :</span>
-        <ul>
-          <li>
-            <span>Développeur Web</span>
-            <Link
-              href="https://openclassrooms.com/fr/paths/717-developpeur-web"
-              target="_blank"
-              aria-label="Visiter la page de formation Développeur Web"
-              title="Visiter la page de formation Développeur Web"
-            >
-              <ExternalLink className={styles.svg} />
-            </Link>
-          </li>
-          <li>
-            <span>Développeur d'application</span>
-            <span>
-              JavaScript React
-              <Link
-                href="https://openclassrooms.com/fr/paths/877-developpeur-dapplication-javascript-react"
-                target="_blank"
-                aria-label="Visiter la page de formation Développeur d'application JavaScript React"
-                title="Visiter la page de formation Développeur d'application JavaScript React"
-              >
-                <ExternalLink className={styles.svg} />
-              </Link>
-            </span>
-          </li>
-        </ul>
-        Je souhaite maintenant trouver une entreprise pour collaborer.
-      </article>
     </section>
   );
 }
