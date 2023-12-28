f1ea16052a4768564607fb0a54ffeb6dac71a044
David Michel <david.michel2809@gmail.com>
2023-12-28 08:44:47 +0100

    feat: add appearance effects on Presentation

--- app/components/Presentation/Presentation.tsx
+++ app/components/Presentation/Presentation.tsx
@@ -1,10 +1,26 @@
+"use client"
+
 import Image from "next/image";
 import picture from "@/public/david-picture.png"
 import styles from "./presentation.module.css";
+import { useEffect, useRef } from "react";
 
 function Presentation() {
+  const presentationElementRef = useRef<HTMLSelectElement>(null!)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      if (!entry.isIntersecting) return
+
+      presentationElementRef.current.classList.add(styles.show)
+      observer.unobserve(presentationElementRef.current)
+    }, { threshold: 0.3 })
+
+    observer.observe(presentationElementRef.current)
+  }, [])
+
   return (
-    <section className={styles.presentation}>
+    <section className={styles.presentation} ref={presentationElementRef}>
       <h1 className={styles.name}>
         <span>David</span>
         <span>Michel</span>
